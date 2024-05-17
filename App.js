d8d91672750007ffbeb6752b382c96d3078a9046
Apoliak7777 <167175652+Apoliak7777@users.noreply.github.com>
2024-05-17 22:32:29 +0200

    Create App.js

---
+++ App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import NoteForm from './components/NoteForm';
+import NoteList from './components/NoteList';
+import './App.css';
+
+function App() {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios.get('/api/notes')
+      .then(response => setNotes(response.data))
+      .catch(error => console.error(error));
+  }, []);
+
+  const addNote = (note) => {
+    axios.post('/api/notes', note)
+      .then(response => setNotes([...notes, response.data]))
+      .catch(error => console.error(error));
+  };
+
+  const deleteNote = (id) => {
+    axios.delete(`/api/notes/${id}`)
+      .then(() => setNotes(notes.filter(note => note._id !== id)))
+      .catch(error => console.error(error));
+  };
+
+  return (
+    <div className="App">
+      <h1>Note Taking App</h1>
+      <NoteForm addNote={addNote} />
+      <NoteList notes={notes} deleteNote={deleteNote} />
+    </div>
+  );
+}
+
+export default App;
